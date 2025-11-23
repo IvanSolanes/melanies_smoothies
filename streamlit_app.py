35639cb8a7b64efad1906ce0b11a258de0fb0677
Ivan <57630705+IvanSolanes@users.noreply.github.com>
2025-11-23 17:07:17 +0100

    Enhance smoothie order with nutrition info display
    Added functionality to display nutrition information for selected fruits and updated the database insertion statement.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,13 @@ if ingredients_list:
     # st.text(ingredients_list)
     ingredients_string=''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-    # st.write(ingredients_string)
+      ingredients_string += fruit_chosen + ' '
+      # st.write(ingredients_string)
+      st.subheader(fruit_chosen + 'Nutrition Information')
+      # New section to display smoothiefroot nutrition information
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+      # st.text(smoothiefroot_response.json())
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """',
@@ -57,8 +62,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-      
-# New section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
